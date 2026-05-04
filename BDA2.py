c201f8c7087d84e0cf4f6aba8972d790e84f3915
a.patil16 <148852399+aditi24patil@users.noreply.github.com>
2026-05-04 09:59:49 +0530

    Update BDA2.py

--- BDA2.py
+++ BDA2.py
@@ -3,40 +3,80 @@ from collections import defaultdict
 
 def mapper(A, B):
     mapped = []
-    rows_A, cols_A, cols_B = len(A), len(A[0]), len(B[0])
+    rows_A, cols_A = len(A), len(A[0])
+    rows_B, cols_B = len(B), len(B[0])
 
+    # Emit A elements
     for i in range(rows_A):
+        for k in range(cols_A):
+            for j in range(cols_B):
+                mapped.append(((i, j), ('A', k, A[i][k])))
+
+    # Emit B elements
+    for k in range(rows_B):
         for j in range(cols_B):
-            for k in range(cols_A):
-                mapped.append((i, j), (A[i][k], B[k][j]))
+            for i in range(rows_A):
+                mapped.append(((i, j), ('B', k, B[k][j])))
+
     return mapped
 
-def reducer(mapped):
-    reduced = defaultdict(list)
-    for key, (a, b) in mapped:
-        result[key] += a * b
+
+def shuffle_sort(mapped):
+    grouped = defaultdict(list)
+    for key, value in mapped:
+        grouped[key].append(value)
+    return grouped
+
+
+def reducer(grouped):
+    result = {}
+
+    for (i, j), values in grouped.items():
+        A_vals = {}
+        B_vals = {}
+
+        # Separate A and B values
+        for tag, k, val in values:
+            if tag == 'A':
+                A_vals[k] = val
+            else:
+                B_vals[k] = val
+
+        # Compute dot product
+        total = 0
+        for k in A_vals:
+            if k in B_vals:
+                total += A_vals[k] * B_vals[k]
+
+        result[(i, j)] = total
+
     return result
 
-#Input
+
+# -------- INPUT --------
 r1, c1 = map(int, input("Enter rows & cols of A: ").split())
-print("Enter Matrx A:")
+print("Enter Matrix A:")
 A = [list(map(int, input().split())) for _ in range(r1)]
 
 r2, c2 = map(int, input("Enter rows & cols of B: ").split())
+
 if c1 != r2:
     print("Multiplication not possible")
     exit()
 
 print("Enter Matrix B:")
-B = [list(map(int, input(). split())) for _ in range(r2)]
+B = [list(map(int, input().split())) for _ in range(r2)]
 
-#Mapreduce
+
+# -------- MAPREDUCE --------
 mapped = mapper(A, B)
-result = reducer(mapped)
+grouped = shuffle_sort(mapped)
+result = reducer(grouped)
 
-#Output
-print("\nResult Matrix: ")
+
+# -------- OUTPUT --------
+print("\nResult Matrix:")
 for i in range(r1):
     for j in range(c2):
-        print(result[(i, j)], end=" ")
+        print(result.get((i, j), 0), end=" ")
     print()
