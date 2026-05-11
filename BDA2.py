3492ba4ed949bad4067cca984a3fea2b88d6b09f
a.patil16 <148852399+aditi24patil@users.noreply.github.com>
2026-05-11 19:50:01 +0530

    Update BDA2.py

--- BDA2.py
+++ BDA2.py
@@ -1,85 +1,46 @@
-#Implement Matrix Multiplication using Map-Reduce
-from collections import defaultdict
+A = [[1,2],
+     [3,4]]
 
-# ---------------- MAPPER ----------------
-def mapper(A, B):
-    mapped = []
-    rows_A, cols_A = len(A), len(A[0])
-    rows_B, cols_B = len(B), len(B[0])
+B = [[5,6],
+     [7,8]]
 
-    # Emit elements of A
+rows_A = len(A)
+cols_A = len(A[0])
+cols_B = len(B[0])
+
+def mapper(A,B):
+    mapped=[]
     for i in range(rows_A):
-        for k in range(cols_A):
-            for j in range(cols_B):
-                mapped.append(((i, j), ('A', k, A[i][k])))
-
-    # Emit elements of B
-    for k in range(rows_B):
         for j in range(cols_B):
-            for i in range(rows_A):
-                mapped.append(((i, j), ('B', k, B[k][j])))
-
+           for k in range(cols_A):
+               mapped.append(((i,j),(A[i][k],B[k][j])))
     return mapped
 
-
-# ---------------- SHUFFLE ----------------
-def shuffle_sort(mapped):
-    grouped = defaultdict(list)
-    for key, value in mapped:
+def shuffle(mapped):
+    grouped={}
+    for key,value in mapped:
+        if key not in grouped:
+            grouped[key]=[]
         grouped[key].append(value)
     return grouped
 
-
-# ---------------- REDUCER ----------------
 def reducer(grouped):
-    result = {}
-
-    for (i, j), values in grouped.items():
-        A_vals = {}
-        B_vals = {}
-
-        # Separate A and B values
-        for tag, k, val in values:
-            if tag == 'A':
-                A_vals[k] = val
-            else:
-                B_vals[k] = val
-
-        # Compute dot product
-        total = 0
-        for k in A_vals:
-            if k in B_vals:
-                total += A_vals[k] * B_vals[k]
-
-        result[(i, j)] = total
-
+    result={}
+    for key in grouped:
+        sum_val=0
+        for a,b in grouped[key]:
+            sum_val+=a*b
+        result[key]=sum_val
     return result
 
+mapped=mapper(A,B)
+grouped=shuffle(mapped)
+result=reducer(grouped)
 
-# ---------------- INPUT ----------------
-r1, c1 = map(int, input("Enter rows & cols of A: ").split())
-print("Enter Matrix A:")
-A = [list(map(int, input().split())) for _ in range(r1)]
-
-r2, c2 = map(int, input("Enter rows & cols of B: ").split())
-
-if c1 != r2:
-    print("Multiplication not possible")
-    exit()
-
-print("Enter Matrix B:")
-B = [list(map(int, input().split())) for _ in range(r2)]
-
-
-# ---------------- MAPREDUCE ----------------
-mapped = mapper(A, B)
-grouped = shuffle_sort(mapped)
-result = reducer(grouped)
-
-
-# ---------------- OUTPUT ----------------
-print("\nResult Matrix:")
-for i in range(r1):
-    for j in range(c2):
-        print(result.get((i, j), 0), end=" ")
-    print()
+C=[[0 for _ in range(cols_B)] for _ in range(rows_A)]
+for (i,j), value in result.items():
+    C[i][j]=value
+print('Result Matrix :')
+for row in C:
+    print(row)
+     
